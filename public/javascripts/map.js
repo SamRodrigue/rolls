0fe7380bb06725c7c5ece49ba2c3ba2c02d1589e
Sam Rodrigue <samrodrigue@hotmail.com>
2018-08-14 21:10:33 -0400

    Added modes for drawing and erasing walls.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,8 +6,8 @@ var map = {
     spacing: 5
   },
   wall: {
-    spacing: 2.5,
-    width: 2
+    spacing: 5,
+    width: 1
   },
   walls: [],
   objects: []
@@ -18,7 +18,11 @@ var view = {
   height: 480,
   x: 50,
   y: 50,
-  zoom: 10
+  zoom: 10,
+  mx: 50,
+  my: 50,
+  wx: 50,
+  wy: 50
 };
 
 var mode = {
@@ -27,7 +31,8 @@ var mode = {
   MAP:    0, // Move map
   WALL:   1, // Modify walls
   OBJECT: 2, // Modify objects
-  COUNT:  3
+  ERASE:  3,
+  COUNT:  4
 }
 
 var wall = null;
@@ -71,9 +76,11 @@ class Wall {
   }
 
   draw() {
+    if (this.mode !== 2) return;
+    sketch.noStroke();
     sketch.fill(0);
+
     if (this.points === null) { // Could be done in end or copy constructor
-      this.points = [];
       var par = {
         x: this.x[1] - this.x[0],
         y: this.y[1] - this.y[0]
@@ -81,26 +88,24 @@ class Wall {
       var mag = Math.sqrt(par.x * par.x + par.y * par.y);
       par.x /= mag;
       par.y /= mag;
-      var pro = {
-        x: -par.y,
-        y: par.x
+      var per = {
+        x: -par.y * map.wall.width / 2,
+        y:  par.x * map.wall.width / 2
       };
-      this.points[0] = {
-        x: this.x[0] + pro.x,
-        y: this.y[0] + pro.y
-      };
-      this.points[1] = {
-        x: this.x[0] - pro.x,
-        y: this.y[0] - pro.y
-      };
-      this.points[2] = {
-        x: this.x[1] - pro.x,
-        y: this.y[1] - pro.y
-      };
-      this.points[3] = {
-        x: this.x[1] + pro.x,
-        y: this.y[1] + pro.y
-      };
+
+      this.points = [{
+        x: this.x[0] + per.x,
+        y: this.y[0] + per.y
+      }, {
+        x: this.x[0] - per.x,
+        y: this.y[0] - per.y
+      }, {
+        x: this.x[1] - per.x,
+        y: this.y[1] - per.y
+      }, {
+        x: this.x[1] + per.x,
+        y: this.y[1] + per.y
+      }];
     }
 
     sketch.quad(
@@ -109,6 +114,70 @@ class Wall {
       this.points[2].x, this.points[2].y,
       this.points[3].x, this.points[3].y
     );
+
+    sketch.ellipse(this.x[0], this.y[0], map.wall.width, map.wall.width);
+    sketch.ellipse(this.x[1], this.y[1], map.wall.width, map.wall.width);
+  }
+
+  drawToMouse(x, y) {
+    if (this.mode !== 1) return;
+    sketch.noStroke();
+    sketch.fill(0);
+    
+    var par = {
+      x: x - this.x[0],
+      y: y - this.y[0]
+    };
+    var mag = Math.sqrt(par.x * par.x + par.y * par.y);
+    par.x /= mag;
+    par.y /= mag;
+    var per = {
+      x: -par.y * map.wall.width / 2,
+      y:  par.x * map.wall.width / 2
+    };
+
+    var points = [
+    {
+      x: this.x[0] + per.x,
+      y: this.y[0] + per.y
+    }, {
+      x: this.x[0] - per.x,
+      y: this.y[0] - per.y
+    }, {
+      x: x - per.x,
+      y: y - per.y
+    }, {
+      x: x + per.x,
+      y: y + per.y
+    }];
+
+    sketch.quad(
+      points[0].x, points[0].y,
+      points[1].x, points[1].y,
+      points[2].x, points[2].y,
+      points[3].x, points[3].y
+    );
+
+    sketch.ellipse(this.x[0], this.y[0], map.wall.width, map.wall.width);
+    sketch.ellipse(x, y, map.wall.width, map.wall.width);
+  }
+
+  contains(x, y) {
+    var v0 = {
+      x: this.points[1].x - this.points[0].x,
+      y: this.points[1].y - this.points[0].y
+    };
+    var v1 = {
+      x: this.points[3].x - this.points[0].x,
+      y: this.points[3].y - this.points[0].y
+    };
+
+    if ((x - this.points[0].x) * v0.x + (y - this.points[0].y) * v0.y < 0) return false;
+    if ((x - this.points[1].x) * v0.x + (y - this.points[1].y) * v0.y > 0) return false;
+    if ((x - this.points[0].x) * v1.x + (y - this.points[0].y) * v1.y < 0) return false;
+    if ((x - this.points[3].x) * v1.x + (y - this.points[3].y) * v1.y > 0) return false;
+
+    return true;
   }
 }
 
@@ -153,20 +222,27 @@ sketch.draw = function () {
 
 sketch.mousePressed = function() {
   if (!onCanvas) return;
-  var x = (sketch.mouseX - sketch.width/2) / view.zoom + view.x;
-  var y = (sketch.mouseY - sketch.height/2) / view.zoom + view.y;
-  x = Math.round(x / map.wall.spacing) * map.wall.spacing;
-  y = Math.round(y / map.wall.spacing) * map.wall.spacing;
   switch (mode.set) {
     case mode.WALL:
       if (wall === null) {
-        wall = new Wall(x, y);
+        wall = new Wall(view.wx, view.wy);
       } else {
-        wall.end(x,y);
+        wall.end(view.wx, view.wy);
         if (wall.mode === 2) {
           map.walls.push(wall.copy());
+          wall = new Wall(view.wx, view.wy);
+        } else {
+          wall = null;
         }
-        wall = null;
+      }
+      break;
+    case mode.ERASE:
+      // Find wall/object below cursor
+      for (var i = map.walls.length - 1; i >= 0; --i) {
+        if (map.walls[i].contains(view.mx, view.my)) {
+          map.walls.splice(i, 1);
+          i = 0;
+        }
       }
       break;
   }
@@ -196,11 +272,15 @@ sketch.keyPressed = function() {
     case 'w':
       sketch.setMode(mode.WALL);
       break;
+    case 'e':
+      sketch.setMode(mode.ERASE);
+      break;
   }
 }
 
 sketch.setMode = function(m) {
   if (m >= 0 && m < mode.COUNT) {
+    wall = null;
     mode.set = m;
   }
 }
@@ -227,6 +307,10 @@ function draw_walls() {
   for (w of map.walls) {
     w.draw();
   }
+
+  if (wall !== null && wall.mode === 1) {
+    wall.drawToMouse(view.wx, view.wy);
+  }
 }
 
 function draw_objects() {
@@ -238,22 +322,30 @@ function draw_players() {
 }
 
 function draw_cursor() {
-  var x = (sketch.mouseX - sketch.width/2) / view.zoom + view.x;
-  var y = (sketch.mouseY - sketch.height/2) / view.zoom + view.y;
+  view.mx = (sketch.mouseX - sketch.width/2) / view.zoom + view.x;
+  view.my = (sketch.mouseY - sketch.height/2) / view.zoom + view.y;
   sketch.rectMode(sketch.CENTER);
 
   switch (mode.set) {
     case mode.MAP:
       sketch.fill(0);
+      sketch.rect(view.mx, view.my, 10 / view.zoom, 10 / view.zoom);
       break;
     case mode.WALL:
+      view.wx = Math.round(view.mx / map.wall.spacing) * map.wall.spacing;
+      view.wy = Math.round(view.my / map.wall.spacing) * map.wall.spacing;
       sketch.fill(0, 255, 255);
-      x = Math.round(x / map.wall.spacing) * map.wall.spacing;
-      y = Math.round(y / map.wall.spacing) * map.wall.spacing;
+      sketch.rect(view.wx, view.wy, 10 / view.zoom, 10 / view.zoom);
+      sketch.fill(0);
+      sketch.rect(view.mx, view.my, 10 / view.zoom, 10 / view.zoom);
+      break;
+    case mode.ERASE:
+      view.wx = Math.round(view.mx / map.wall.spacing) * map.wall.spacing;
+      view.wy = Math.round(view.my / map.wall.spacing) * map.wall.spacing;
+      sketch.fill(255, 0, 0);
+      sketch.rect(view.mx, view.my, 10 / view.zoom, 10 / view.zoom);
       break;
   }
-
-	sketch.rect(x, y, 10 / view.zoom, 10 / view.zoom);
 }
 }; // End of sketch
 
